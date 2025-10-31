b47c3e928da3633576f7bbaa809600788549eb62
Andrii <dikij.andrej@gmail.com>
2025-10-31 11:27:46 +0200

    add helmet

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import 'dotenv/config';
+import helmet from 'helmet';
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(express.json());
 app.use(
   pino({
     level: 'info',
